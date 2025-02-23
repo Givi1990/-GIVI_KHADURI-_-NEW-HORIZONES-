0ff9b2daeb2fab97462e4374f15708c2b638c962
Givi <Givi.givi.khaduri@gmail.com>
2025-02-23 19:51:46 +0400

    7 commit

--- src/app/SERVICES/LOGIN-REGISTRATION/login-registration.service.ts
+++ src/app/SERVICES/LOGIN-REGISTRATION/login-registration.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable, of } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { UserRegistration } from '../../INTERFACE/registation.interface';
 import { UserLogin } from '../../INTERFACE/login.interfece';
 
@@ -8,9 +8,6 @@ import { UserLogin } from '../../INTERFACE/login.interfece';
   providedIn: 'root'
 })
 export class LoginRegistrationService {
-
-  private isLoginSubject = new BehaviorSubject<boolean>(false);  // BehaviorSubject для отслеживания состояния входа
-  isLogin$ = this.isLoginSubject.asObservable(); // Это можно подписывать в компонентах
 
   private apiUser = "https://task4-2d93e-default-rtdb.europe-west1.firebasedatabase.app/users.json";
 
@@ -20,31 +17,29 @@ export class LoginRegistrationService {
     return this.http.get<any>(this.apiUser);
   }
 
-  checkIsLogin(): Observable<boolean> {
-    const user = sessionStorage.getItem("user");
-    const isLogin = user === 'true';
-    this.isLoginSubject.next(isLogin); 
-    console.log("serv:",isLogin);
-    
-    return of(isLogin);
-  }
+
+  
 
   loginUser(data: UserLogin): Observable<UserRegistration> {
     return this.getUsers().pipe(
       map(users => {
+        console.log('Fetched users:', users);
+  
         if (!users) {
           throw new Error("No users found in the database.");
         }
-
+  
         const { userName, password } = data;
-        let usersArray: UserRegistration[] = [];
-
+  
+        let usersArray: UserRegistration[];
         if ((users as any).userName) {
           usersArray = [users as UserRegistration];
         } else {
           usersArray = Object.values(users) as UserRegistration[];
         }
-
+  
+        console.log('Users array:', usersArray);
+  
         let foundUser: UserRegistration | null = null;
         for (const user of usersArray) {
           if (user.userName === userName && user.password === password) {
@@ -52,15 +47,20 @@ export class LoginRegistrationService {
             break;
           }
         }
-
+  
+        console.log('Found user:', foundUser);
+  
         if (!foundUser) {
           throw new Error("Invalid user name or password");
         }
-
+  
         return foundUser;
       })
     );
   }
+  
+  
+
 
   checkUserExists(userName: string): Observable<boolean> {
     return this.getUsers().pipe(
@@ -70,13 +70,10 @@ export class LoginRegistrationService {
       })
     );
   }
+  
 
   registerUser(user: UserRegistration): Observable<any> {
     return this.http.put(this.apiUser, user);
   }
-
-  // Устанавливаем состояние входа в BehaviorSubject
-  setLoginState(isLoggedIn: boolean): void {
-    this.isLoginSubject.next(isLoggedIn);
-  }
+  
 }
